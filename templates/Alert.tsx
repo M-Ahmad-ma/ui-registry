8306b6007f9554c6a53c8968b5581c672269ca62
M-Ahmad-ma <muhammadahmad@gmail.com>
2025-09-17 07:46:28 +0000

    commit

---
+++ templates/Alert.tsx
@@ -0,0 +1,50 @@
+
+import * as React from "react"
+import { cn } from "@/lib/utils/cn"
+
+export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  variant?: "default" | "destructive"
+}
+
+function Alert({ className, variant = "default", ...props }: AlertProps) {
+  return (
+    <div
+      role="alert"
+      className={cn(
+        "relative w-full rounded-lg border p-4",
+        variant === "default" &&
+          "bg-background text-foreground border-border",
+        variant === "destructive" &&
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref as any}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
