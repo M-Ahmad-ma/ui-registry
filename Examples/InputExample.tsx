2b8a3c069f527c043c3b85f5dc1a0b9861a645e7
M-Ahmad-ma <muhammadahmad@gmail.com>
2025-09-18 14:35:26 +0000

    commit

--- Examples/InputExample.tsx
+++ Examples/InputExample.tsx
@@ -6,10 +6,6 @@ export default function InputExample() {
   return (
     <div className="space-y-4 p-6 max-w-md">
       <Input placeholder="Default input" />
-      <Input placeholder="Disabled input" disabled />
-      <Input placeholder="Password" type="password" />
-      <Input placeholder="Email address" type="email" />
-      <Input placeholder="With default value" defaultValue="Hello" />
     </div>
   )
 }
