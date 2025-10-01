ae5e7aef98e5f34a925995bb15cf716ad9a15521
M-Ahmad-ma <muhammadahmad@gmail.com>
2025-10-01 11:01:46 +0000

    batman

--- templates/Breadcrumb.tsx
+++ templates/Breadcrumb.tsx
@@ -3,7 +3,7 @@
 
 import * as React from "react"
 import { ChevronRight, MoreHorizontal } from "lucide-react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/cn"
 
 interface BreadcrumbProps extends React.HTMLAttributes<HTMLElement> {
   separator?: React.ReactNode
