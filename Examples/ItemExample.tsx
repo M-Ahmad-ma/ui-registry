ac7f85019447603153f4dc7a3ab914d1d4446d45
M-Ahmad-ma <muhammadahmad@gmail.com>
2025-10-06 07:30:43 +0000

    uwu

---
+++ Examples/ItemExample.tsx
@@ -0,0 +1,46 @@
+
+import {
+  Item,
+  ItemGroup,
+  ItemHeader,
+  ItemMedia,
+  ItemContent,
+  ItemTitle,
+  ItemDescription,
+  ItemActions,
+  ItemSeparator,
+  ItemFooter,
+} from "@/components/ui/Item"
+
+import { Button } from "@/components/ui/Button"
+import { Star } from "lucide-react"
+
+export default function ItemExample() {
+  return (
+    <ItemGroup>
+      <Item>
+        <ItemHeader>
+          <ItemMedia variant="icon">
+            <Star />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Premium Feature</ItemTitle>
+            <ItemDescription>Unlock access to exclusive tools</ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <Button size="sm">Upgrade</Button>
+          </ItemActions>
+        </ItemHeader>
+      </Item>
+
+      <ItemSeparator />
+
+      <Item variant="muted">
+        <ItemContent>
+          <ItemTitle>Free Tier</ItemTitle>
+          <ItemDescription>Limited access to basic tools</ItemDescription>
+        </ItemContent>
+      </Item>
+    </ItemGroup>
+  )
+}
